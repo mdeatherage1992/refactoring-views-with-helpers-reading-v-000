cfc5cb4c6f2e0c4ebae6d32a331ac10677657564
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-14 12:50:39 +0000

    automatically backed up by learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
-# module ApplicationHelper
-# end
+module ApplicationHelper
+
 
 def title(text)
   content_for :title, text
 end
+
+<% title @post.title %>
+ 
+<h1><%= @post.title %></h1>
+<p><%= last_updated @post %></p>
+<p><%= @post.description %></p>
